256ea10e99c4d8295ac6f701aab6855a47645686
geeta-seshapalli <geetaseshapalli@gmail.com>
2025-07-10 12:38:09 +0530

    TodoAPI Spring Boot Demo

---
+++ TodoAPISpringBoot/src/main/java/org/example/todoapispringboot/TodoApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.todoapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApiSpringBootApplication.class, args);
+    }
+
+}
---
+++ TodoAPISpringBoot/src/test/java/org/example/todoapispringboot/TodoApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.todoapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
